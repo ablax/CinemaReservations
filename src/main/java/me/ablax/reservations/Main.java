52c38c75fd51c004704bdbf473e4184a840817fd
Мurаd Hаmzа <muradmura59@gmail.com>
2023-06-22 13:52:18 +0300

    Initial commit

---
+++ src/main/java/me/ablax/reservations/Main.java
@@ -0,0 +1,13 @@
+package me.ablax.reservations;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
